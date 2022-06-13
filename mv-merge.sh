991feee6304fd42ac05bc9aebeb3f5ef9c497b27
Thue  Vestergaard Iversen <tvi@moonskye.com>
2022-06-13 09:06:37 +0200

    Added mv-merge script

---
+++ mv-merge.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+pwd=$(pwd)
+
+cleanpath() {
+  local lpwd=$(pwd)
+  cd "$1" 2> /dev/null || { echo "Path not valid"; exit 1; }
+  local returnVal=$(pwd);
+  cd lpwd || { echo "The originating path does no longer exist..."; exit 1; }
+  echo "${returnVal}"
+}
+
+SOURCE=$(cleanpath ${1});
+DESTINATION=$(cleanpath ${2});
+
+echo Source: "${SOURCE}"
+echo Destination: "${DESTINATION}"
+exit;
+cd "${SOURCE}" || { echo "Source folder suddenly does not exist anymore..." ; exit 1;}
+find . -type d -exec echo Creating folder {} \; -exec mkdir -p "${DESTINATION}"/{} \;
+find . -type f -exec echo Moving {} \; -exec mv -u {} "${DESTINATION}"/{} \;
+cd "${pwd}" || { echo "The start path no longer exists..."; exit 1; }
+
+read -p "Moving complete, delete source folder(s)? (yes/no) " yn
+while true; do
+  case $yn in
+    [yY]* ) echo "Removing source...";
+      rm -rf ${SOURCE};
+      echo "Done!";
+      exit 0;;
+    [nN]* ) echo "Skipping, done!";
+      exit 0;;
+    * ) echo invalid response;
+      exit 1;;
+  esac
+done
